faa8d7158ceb13524e50501281771969ec04cd8e
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-10-18 17:10:59 -0400

    compositionCard logic map future reference

--- components/CompositionCard.jsx
+++ components/CompositionCard.jsx
@@ -1,10 +1,16 @@
+// coditional logic map:
+// if (links.length === 1), return the name as an <a> tag wrapped with <q> tag, allowing for "display: inline" (on the parent <h3>) to be inherited on the quotations
+// possible alternative: wrap <a> tag with <p> tag container quotations, change <a> to be "display: inline-block" on the parent
+// if (links.length === 2), return the composition name as a <p> tag with its innerHTML including the quotations, then display hyperlinks in "(part 1, part 2)" format
+// if (linnks.length === 0), return <p> tag containing quotes around name, no hyperlinks
+// ALL CASES: return "space" HTML entity(&nbsp;) after name, before <span>{keywords}</span> to avoid line-break depending on <h3>s' "margin: ...", avoiding rivers between text at line-start
+
 export default function CompositionCard({ composition }){
     const { name, keywords, details, links, quotes, description } = composition;
 
     return (
         <div className="composition-card">
             <h3>
-                {/* {links.length === 1 ? '"' : null} */}
                 {links.length === 1 ? <q><a target="_blank" href={links[0]}>{name}</a></q>
                 : links.length === 2 ? <>{`"${name}"`}&nbsp;{`(`}<a href={links[0]}>part 1</a>,&nbsp;<a href={links[1]}>part 2</a>{`)`}&nbsp;&nbsp;</>
                 : <>"{name}"&nbsp;&nbsp;</>}
