9a98126f45bf18c78689cc34d18c7c3e8f5d43b3
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-10-22 12:22:51 -0400

    next-auth installation/client login components

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,11 +1,13 @@
 import Landing from "@/components/Landing";
 import blogs from "@/util/blogs";
+import LoginButtons from "@/components/loginButtons";
 
 export default function Blog(){
     return(
         <div id="blog">
             <Landing name="Blog" imageUrl="/blog-landing.jpg"/>
             <div id="blog-content">
+                <LoginButtons />
                 {blogs.index["2023"].map((value, i) => 
                     <div>
                         <h2>{2023}</h2>
---
+++ components/loginButtons.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { signIn } from "next-auth/react"
+
+export default function LoginButtons(){
+    return (
+        <p onClick={() => {
+            signIn("google");
+        }}></p>
+    )
+}
