84f21baf7916723dcbda76c7bb5c8abcbbbcf8cf
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-10-23 17:02:07 -0400

    contact post test fix

--- app/contact/page.js
+++ app/contact/page.js
@@ -25,7 +25,7 @@ export default function Contact(){
             return setStatus("invalid");
         } else {
             try {
-                const response = await fetch('https://mysteriesandmusic.com/api/post-email', {
+                const response = await fetch('https://www.mysteriesandmusic.com/api/post-email', {
                     method: 'POST',
                     body: JSON.stringify(body),
                     headers: {
