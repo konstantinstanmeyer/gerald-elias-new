1e2856ac23012631037179fd16266827369b1c05
KonstantinStanmeyer <konstantin@stanmeyer.com>
2024-02-07 17:47:41 -0500

    connections update live version

--- app/connections/page.js
+++ app/connections/page.js
@@ -7,9 +7,22 @@ export default function Connections(){
             <div id="connections-content">
                 <h2>Past Media Features and Ways to Connect</h2>
                 <div id="connections-list">
+                    <h3>January 2024</h3>
+                    <p>Read Gerald's blog post for #WriterWednesday on author Heather Weidner's website.&nbsp;<a target="_blank" href="https://www.heatherweidner.com/blog/2024/1/3/writerwednesday-interview-with-gerald-elias">here</a></p>
                     <h3>December 2023</h3>
                     <p>Listen to Gerald's interview on NBC about his book "Murder at the Royal Albert".&nbsp;<a target="_blank" href="https://shows.acast.com/houseofmysteryradio/episodes/gerald-elias-murder-at-the-royal-albert">here</a></p>
                     <p>Enjoy learning about the blending of writing and the life of a professional concert musician from Gerald Elias.&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=hl2H3i9yF-g">here</a></p>
+                    <p>Learn more about the inner workings of Gerald's book, "Murder at the Royal Albert," and more! Interviewed by Alan Warren.&nbsp;<a target="_blank" href="https://open.spotify.com/episode/6dneVCl0qUGq6w7IwGLJCk">here</a></p>
+                    <h3>February 2023</h3>
+                    <p>Carol Goodman Kaufman dives deep into music and mystery with Gerald, also discussing the Daniel Jacobus series.&nbsp;<a target="_blank" href="https://open.spotify.com/episode/3KlAOvhzJpOjRAiJtytqcS">here</a></p>
+                    <h3>December 2020</h3>
+                    <p>An interview with Shawn Reilly Simmons, "Five Compelling Questions with Shawn".&nbsp;<a target="_blank" href="https://podcasts.apple.com/us/podcast/ep2-5cq-for-gerald-elias-musician-and-author-of/id1543612992?i=1000501507065">here</a></p>
+                    <h3>October 2020</h3>
+                    <p>Explore literary and musical insight by watching a dynamic podcast on "ON MIC with JORDAN RICH".&nbsp;<a target="_blank" href="https://onmicwithjordanrich.blubrry.net/2020/10/11/gerald-elias-167/">here</a></p>
+                    <h3>December 2019</h3>
+                    <p>Gerald Elias Performs his award-winning essay, “War and Peace. And Music,” at TEDxSaltLakeCity.&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=V2S6LuBAus4">here</a></p>
+                    <h3>May 2015</h3>
+                    <p>"Music to Die For": Elias chats with host Steven Kapp Perry on BYUradio's podcast, "Highway 89".&nbsp;<a target="_blank" href="https://open.spotify.com/episode/1xeV6CzVstaTEBzSwHxBLk">here</a></p>
                 </div>
             </div>
         </div>
