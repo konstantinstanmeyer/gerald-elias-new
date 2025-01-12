c74e02752e3c43f76b52f728a29ec007590d6d5a
KonstantinStanmeyer <konstantin@stanmeyer.com>
2025-01-11 22:33:16 -0500

    folio performance march 2025 added to events page

--- util/events.js
+++ util/events.js
@@ -1,11 +1,36 @@
 const events = [
     {
-        "date": "April 11th-14th, 2024",
-        "name":"Left Coast Crime 2024: Seattle Shakedown mystery book convention",
-        "location":"Hyatt Regency Bellevue",
-        "address":"900 Bellevue Way NE, Bellevue, WA 98004",
-        "imageUrl":"left-coast-crime.png",
-        "hyperlink":"https://leftcoastcrime.org/2024/index.html"
+        "date": "Sunday, November 10th, 2024 @ 3:00-4:15PM followed by reception",
+        "name": `Violin Recital, "A Classical Kaleidoscope"`,
+        "location":"Folio Anthenaeum Library, 93 Park St. #307, Seattle, WA",
+        "description":"Joined by pianist Michelle Huang. A selection of virtuoso violin repertoire found in Elias's Daniel Jacobus mystery series",
+        "imageUrl":"kaleidoscope.png",
+        "hyperlink":"https://www.folioseattle.org/event-details/a-classical-kaleidoscope"
+    },
+    {
+        "date": "Saturday, November 2nd, 2024 @ 1PM",
+        "name":"Joint Book Presentation",
+        "location":"The Seattle Public Library - West Seattle",
+        "address":"2306 42nd Ave SW, Seattle, WA 98116",
+        "description":"Joined by authors Erica Miner and Charles Philipp Martin. A discussion and music about the authors' recently released mysteries",
+        "imageUrl":"west-seattle.jpeg",
+        "hyperlink":"https://www.spl.org/hours-and-locations/west-seattle-branch"
+    },
+    {
+        "date": "Friday, October 25th, 2024 @ 7:00-8:00PM",
+        "name":"Interview and Performance on Northwest Focus",
+        "location":"KING-FM Radio, Seattle, WA",
+        "description":"Discussion with host Dave Beck about Elias's music and literary career, including performance of selections from his upcoming violin recitals.",
+        "imageUrl":"king-fm.png",
+    },
+    {
+        "date": "Wednesday, August 14th, 2024 @ 4:00-6:00PM",
+        "name":"Book Presentation with Music",
+        "location":"West Stockbridge Old Town Hall",
+        "address":"9 Main St, West Stockbridge, MA 01266",
+        "description":"Daniel Jacobus series and Murder on Vacation presented by Friends of the Library. Reception and book signing afterwards!",
+        "imageUrl":"west-stockbridge-event.png",
+        "hyperlink":"https://weststockbridgepl.org/about-us/"
     },
     {
         "date": "Monday, July 22nd, 2024 @ 5:30PM",
@@ -15,6 +40,31 @@ const events = [
         "imageUrl":"the-bookstore.png",
         "description":"In conversation with Kaufman about her novel, The First Murder and Elias's most recent Daniel Jacobus mystery, Murder at the Royal Albert.",
         "hyperlink":"https://www.bookstoreinlenox.com/events/"
+    },
+    {
+        "date": "Saturday, June 8th, 2024 @ 10:00AM",
+        "name":"Book presentation with music, Murder at the Royal Albert",
+        "location":"Inglewood Golf Club (WA)",
+        "address":"900 Bellevue Way NE, Bellevue, WA 98004",
+        "imageUrl":"inglewood-golf-club.jpeg",
+        "hyperlink":"https://leftcoastcrime.org/2024/index.html"
+    },
+    {
+        "date":"Thursday, May 16th, 2024 @ 3:30-5:00PM PST",
+        "name":"Mystery & Music at Oak Harbor Library",
+        "location":"Oak Harbor Library",
+        "address":"1000 SE Regatta Dr, Oak Harbor, WA 98277",
+        "imageUrl":"IG-MysteryAndMusic.jpg",
+        "description":"Critically acclaimed author and musician Gerald Elias blends music and mystery for a presentation to indulge the senses! He will discuss his books and play snippets of pieces featured in his novels.",
+        "hyperlink":"https://sno-isle.bibliocommons.com/events/6579ff940825261328f3514c"
+    },
+    {
+        "date": "April 11th-14th, 2024",
+        "name":"Left Coast Crime 2024: Seattle Shakedown mystery book convention",
+        "location":"Hyatt Regency Bellevue",
+        "address":"900 Bellevue Way NE, Bellevue, WA 98004",
+        "imageUrl":"left-coast-crime.png",
+        "hyperlink":"https://leftcoastcrime.org/2024/index.html"
     },
     {
         "date":"Thursday, February 29th, 2024 @ 3:30PM PST",
@@ -34,54 +84,12 @@ const events = [
         "description":"Joined by Gus Denhard on theorbo.",
     },
     {
-        "date":"Thursday, May 16th, 2024 @ 3:30-5:00PM PST",
-        "name":"Mystery & Music at Oak Harbor Library",
-        "location":"Oak Harbor Library",
-        "address":"1000 SE Regatta Dr, Oak Harbor, WA 98277",
-        "imageUrl":"IG-MysteryAndMusic.jpg",
-        "description":"Critically acclaimed author and musician Gerald Elias blends music and mystery for a presentation to indulge the senses! He will discuss his books and play snippets of pieces featured in his novels.",
-        "hyperlink":"https://sno-isle.bibliocommons.com/events/6579ff940825261328f3514c"
-    },
-    {
-        "date": "Saturday, June 8th, 2024 @ 10:00AM",
-        "name":"Book presentation with music, Murder at the Royal Albert",
-        "location":"Inglewood Golf Club (WA)",
-        "address":"900 Bellevue Way NE, Bellevue, WA 98004",
-        "imageUrl":"inglewood-golf-club.jpeg",
-        "hyperlink":"https://leftcoastcrime.org/2024/index.html"
-    },
-    {
-        "date": "Wednesday, August 14th, 2024 @ 4:00-6:00PM",
-        "name":"Book Presentation with Music",
-        "location":"West Stockbridge Old Town Hall",
-        "address":"9 Main St, West Stockbridge, MA 01266",
-        "description":"Daniel Jacobus series and Murder on Vacation presented by Friends of the Library. Reception and book signing afterwards!",
-        "imageUrl":"west-stockbridge-event.png",
-        "hyperlink":"https://weststockbridgepl.org/about-us/"
-    },
-    {
-        "date": "Friday, October 25th, 2024 @ 7:00-8:00PM",
-        "name":"Interview and Performance on Northwest Focus",
-        "location":"KING-FM Radio, Seattle, WA",
-        "description":"Discussion with host Dave Beck about Elias's music and literary career, including performance of selections from his upcoming violin recitals.",
-        "imageUrl":"king-fm.png",
-    },
-    {
-        "date": "Saturday, November 2nd, 2024 @ 1PM",
-        "name":"Joint Book Presentation",
-        "location":"The Seattle Public Library - West Seattle",
-        "address":"2306 42nd Ave SW, Seattle, WA 98116",
-        "description":"Joined by authors Erica Miner and Charles Philipp Martin. A discussion and music about the authors' recently released mysteries",
-        "imageUrl":"west-seattle.jpeg",
-        "hyperlink":"https://www.spl.org/hours-and-locations/west-seattle-branch"
-    },
-    {
-        "date": "Sunday, November 10th, 2024 @ 3:00-4:15PM followed by reception",
-        "name": `Violin Recital, "A Classical Kaleidoscope"`,
-        "location":"Folio Anthenaeum Library, 93 Park St. #307, Seattle, WA",
-        "description":"Joined by pianist Michelle Huang. A selection of virtuoso violin repertoire found in Elias's Daniel Jacobus mystery series",
-        "imageUrl":"kaleidoscope.png",
-        "hyperlink":"https://www.folioseattle.org/event-details/a-classical-kaleidoscope"
+        "date":"Monday, March 3rd, 2025",
+        "name":"Händel and Friends: A Baroque Tapestry",
+        "location":"The Seattle Athenaeum",
+        "description": "Join Gerald Elias and cellist Meeka Quan DiLorenzo at the Seattle Athenaeum for a captivating recital celebrating the brilliance and lasting influence of Georg Frideric Händel.",
+        "imageUrl":"folio2.jpeg",
+        "hyperlink":"https://www.folioseattle.org/event-details/handel-and-friends-a-baroque-tapestry"
     },
     {
         "date":"Friday, December 15th, 2023",
