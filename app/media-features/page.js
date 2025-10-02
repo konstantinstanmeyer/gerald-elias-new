15a0266a74f870afb65b681a7537e3aaab02aad9
KonstantinStanmeyer <konstantin@stanmeyer.com>
2025-10-02 19:45:35 -0400

    punctuation fixes — media features page

--- app/media-features/page.js
+++ app/media-features/page.js
@@ -7,19 +7,19 @@ export default function Connections(){
             <div id="connections-content">
                 <div id="connections-list">
                     <h3>October 2025</h3>
-                    <p>A podcast with Gerald Elias about the role of music as an instrument of international diplomacy. <a target="_blank" href="https://www.youtube.com/watch?v=yYLVlgLTHdo">here</a></p>
+                    <p>A podcast with Gerald Elias about the role of music as an instrument of international diplomacy.&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=yYLVlgLTHdo">here</a></p>
                     <h3>September 2025</h3>
-                    <p>A reading of Chapter 1 of Murder at the Royal Albert on Kings River Life Mysteryrat's Maze Podcast. <a target="_blank" href="https://mysteryratsmaze.podbean.com/e/murder-at-the-royal-albert-by-gerald-elias/">here</a></p>
+                    <p>A reading of Chapter 1 of Murder at the Royal Albert on Kings River Life Mysteryrat's Maze Podcast.&nbsp;<a target="_blank" href="https://mysteryratsmaze.podbean.com/e/murder-at-the-royal-albert-by-gerald-elias/">here</a></p>
                     <h3>January 2024</h3>
                     <p>Read Gerald's blog post for #WriterWednesday on author Heather Weidner's website.&nbsp;<a target="_blank" href="https://www.heatherweidner.com/blog/2024/1/3/writerwednesday-interview-with-gerald-elias">here</a></p>
                     <h3>December 2023</h3>
-                    <p>A second round of reviewing Gerald's books by Viridan Tea House&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=q0MkOnWvOig&t=348s">here</a></p>
+                    <p>A second round of reviewing Gerald's books by Viridan Tea House.&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=q0MkOnWvOig&t=348s">here</a></p>
                     <p>Discover the blending of writing and the life of a professional concert musician from Gerald Elias. Live recording from Stitch.net&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=hl2H3i9yF-g">here</a></p>
                     <p>Listen to Gerald's interview on NBC about his book "Murder at the Royal Albert".&nbsp;<a target="_blank" href="https://shows.acast.com/houseofmysteryradio/episodes/gerald-elias-murder-at-the-royal-albert">here</a></p>
                     <p>Learn more about the inner workings of Gerald's book, "Murder at the Royal Albert," and more! Interviewed by Alan Warren.&nbsp;<a target="_blank" href="https://open.spotify.com/episode/6dneVCl0qUGq6w7IwGLJCk">here</a></p>
                     <h3>September 2023</h3>
                     <p>Watch a short spotlight on Gerald's book "The Devil's Trill".&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=uF1tMWBmSH0">here</a></p>
-                    <p>YouTube channel "Viridian Tea House"'s thoughtful reviews of books by Gerald Elias&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=YsmFv6nE1GA">here</a></p>
+                    <p>YouTube channel "Viridian Tea House"'s thoughtful reviews of books by Gerald Elias.&nbsp;<a target="_blank" href="https://www.youtube.com/watch?v=YsmFv6nE1GA">here</a></p>
                     <h3>February 2023</h3>
                     <p>Carol Goodman Kaufman dives deep into music and mystery with Gerald, also discussing the Daniel Jacobus series.&nbsp;<a target="_blank" href="https://open.spotify.com/episode/3KlAOvhzJpOjRAiJtytqcS">here</a></p>
                     <h3>January 2022</h3>
