075f3616de8b223157ae8e9a40c1f10695bb9b10
KonstantinStanmeyer <konstantin@stanmeyer.com>
2025-12-25 11:27:57 -0500

    composition page css + jsx modernization

--- app/composing/page.js
+++ app/composing/page.js
@@ -3,9 +3,10 @@ import Landing from "@/components/Landing";
 
 export default function Composing(){
     return (
-        <div id="composing">
-            <Landing name="Composing" imageUrl="composing.jpeg" />
-            <div id="composing-content">
+        <main id="composing">
+            <Landing name="Composing" imageUrl="/composing.jpeg" />
+        
+            <section id="composing-content">
                 <div className="composition-card">
                     <h1 className="composition-header">Original Compositions and Transcriptions by Gerald Elias</h1>
                     <h5>Parts, recordings, and reviews available upon request</h5>
@@ -14,41 +15,54 @@ export default function Composing(){
                     <p>Brahms</p>
                     <p>Mozart No. 3 in G Major</p>
                     <p className="section-bottom">Mozart No. 5 in A Major</p>
-                    
                     <h3>Transcriptions for Violin and Piano</h3>
-                    <p>Bartok, &quot;Intermezzo Interrotto&quot; from Concerto for Orchestra</p>
-                    <p>Bazzini, &quot;Second Round of the Goblins&quot; (a parody for 4 violins)</p>
-                    <p>Amy Beach, "Three Browning Songs"  (violin solo)</p>
-                    <p>Faure, &quot;Pavane&quot;</p>
-                    <p>Gottschalk, &quot;Pasquinade&quot; (also arranged for 8 flutes and orchestra)</p>
-                    <p>Kodaly, &quot;Intermezzo&quot; from the Hary Janos Suite</p>
-                    <p>Mozart, &quot;Papageno&#39;s Aria&quot;</p>
-                    <p>Rossini, &quot;Ai Caprici,&quot; from L&#39;Italiana in Algeri</p>
-                    <p>Schubert, &quot;Death and the Maiden&quot; (violin solo)</p>
-                    <p>Sir Arthur Sullivan, &quot;The Moon &amp; I&quot; from the Mikado</p>
-                    <p>Traditional, &quot;El Condor Pasa&quot; (violin solo)</p>
-                    <p className="section-bottom">William Walton, &quot;Touch Her Soft Lips and Part,&quot; from the film Henry V.</p>
-
+                    <p>Bartok, "Intermezzo Interrotto" from Concerto for Orchestra</p>
+                    <p>Bazzini, "Second Round of the Goblins" (a parody for 4 violins)</p>
+                    <p>Amy Beach, "Three Browning Songs" (violin solo)</p>
+                    <p>Faure, "Pavane"</p>
+                    <p>Gottschalk, "Pasquinade" (also arranged for 8 flutes and orchestra)</p>
+                    <p>Kodaly, "Intermezzo" from the Hary Janos Suite</p>
+                    <p>Mozart, "Papageno's Aria"</p>
+                    <p>Rossini, "Ai Caprici," from L'Italiana in Algeri</p>
+                    <p>Schubert, "Death and the Maiden" (violin solo)</p>
+                    <p>Sir Arthur Sullivan, "The Moon & I" from the Mikado</p>
+                    <p>Traditional, "El Condor Pasa" (violin solo)</p>
+                    <p className="section-bottom">William Walton, "Touch Her Soft Lips and Part," from the film Henry V.</p>
                     <h3>Chamber Music</h3>
-                    <p>String Quartet No. 1: &quot;When Mr. Mozart Went to the Ball.&quot; Variations (a la Young Persons Guide to the Orchestra by Benjamin Britten) on Mozart&#39;s Violin Sonata in G Major, K.379, for string quartet and narrators</p>
+                    <p>
+                        String Quartet No. 1: "When Mr. Mozart Went to the Ball." Variations (a la 
+                        Young Persons Guide to the Orchestra by Benjamin Britten) on Mozart's Violin 
+                        Sonata in G Major, K.379, for string quartet and narrators
+                    </p>
                     <p>String Quartet No. 2</p>
-                    <p>&quot;Mack the Knife&quot; Fantasy for String Quartet</p>
-                    <p>&quot;V&#39;adoro&quot; Variations for Violin Solo and tape (Handel&#39;s &quot;V&#39;adoro Pupille)</p>
-                    <p>&quot;William Grandstaff,&quot; Operatic Scene for 3 voices, violin, flute, and piano</p>
-                    <p>&quot;The Legend of William Grandstaff&quot; for baritone, string bass and piano</p>
-                    <p>&quot;Conversations with Essie&quot; for female narrator and chamber ensemble</p>
-                    <p>&quot;Piccolino&#39;s Sarabanda&quot; for solo violin</p>
-                    
-                    <p className="section-bottom">"Dear Great Salt Lake," for narrator, violin, flute, and cimbalom. (Co-composed with Igor Iachimciuc, poem by Raisa Tolchinsky)</p>
+                    <p>"Mack the Knife" Fantasy for String Quartet</p>
+                    <p>"V'adoro" Variations for Violin Solo and tape (Handel's "V'adoro Pupille)</p>
+                    <p>"William Grandstaff," Operatic Scene for 3 voices, violin, flute, and piano</p>
+                    <p>"The Legend of William Grandstaff" for baritone, string bass and piano</p>
+                    <p>"Conversations with Essie" for female narrator and chamber ensemble</p>
+                    <p>"Piccolino's Sarabanda" for solo violin</p>
+                    <p className="section-bottom">
+                        "Dear Great Salt Lake," for narrator, violin, flute, and cimbalom. 
+                        (Co-composed with Igor Iachimciuc, poem by Raisa Tolchinsky)
+                    </p>
                     <h3>Orchestra</h3>
-                    <p>Transcription of the Aaron Copland Sonata for violin and orchestra. [Pub. Boosey &amp; Hawkes]</p>
-                    <p>“The Raven,” Monodrama for Baritone and Orchestra (also a chamber music version for voice, viola, and piano), based upon the poem by Edgar Allan Poe</p>
+                    <p>
+                        Transcription of the Aaron Copland Sonata for violin and orchestra. 
+                        [Pub. Boosey & Hawkes]
+                    </p>
+                    <p>
+                        "The Raven," Monodrama for Baritone and Orchestra (also a chamber music version 
+                        for voice, viola, and piano), based upon the poem by Edgar Allan Poe
+                    </p>
                     <p>Concerto Grosso for Violin and Chamber Orchestra (also for violin and piano)</p>
                     <p>Overture in the Classical Style</p>
-                    <p>Transcription of &quot;Devil&#39;s Trill&quot; Sonata for string orchestra</p>
-                    <p>Transcription of the Andante from Bach&#39;s Violin Sonata in A Minor for string orchestra (can be used as a 2nd mvt. in the 3rd Brandenburg Concerto)</p>
+                    <p>Transcription of "Devil's Trill" Sonata for string orchestra</p>
+                    <p>
+                        Transcription of the Andante from Bach's Violin Sonata in A Minor for string 
+                        orchestra (can be used as a 2nd mvt. in the 3rd Brandenburg Concerto)
+                    </p>
                 </div>
-            </div>
-        </div>
+            </section>
+        </main>
     )
 }
