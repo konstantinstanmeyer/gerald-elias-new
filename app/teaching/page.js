14e286695e431c2928e4b4bd55bb1edf2d5e306d
KonstantinStanmeyer <konstantin@stanmeyer.com>
2025-12-25 11:38:19 -0500

    teaching page priority load + css added

--- app/teaching/page.js
+++ app/teaching/page.js
@@ -1,20 +1,24 @@
+import Image from 'next/image';
 import Landing from '@/components/Landing';
 import textArr from '@/util/teaching';
 
 export default function Teaching() {
   return (
     <main id="teaching">
-      <Landing name="Teaching" imageUrl="/teaching.jpg" />
-      <section id="teaching-content">
-        <img 
-          id="teaching-image" 
-          src="/teaching-profile.jpg" 
-          alt="Gerald Elias Teaching"
+        <Landing name="Teaching" imageUrl="/teaching.jpg" />
+        <section id="teaching-content">
+        <Image 
+            id="teaching-image" 
+            src="/teaching-profile.jpg" 
+            alt="Gerald Elias Teaching"
+            width={280}
+            height={373}
+            priority
         />
         {textArr.map((text, index) => (
-          <p key={`teaching-${index}`}>{text}</p>
+            <p key={`teaching-${index}`}>{text}</p>
         ))}
-      </section>
+        </section>
     </main>
   );
 }
