cf820648413b9afb49d29d452c50c66369a0b043
KonstantinStanmeyer <konstantin@stanmeyer.com>
2025-12-31 09:24:27 -0500

    typing fix + user post route setup

---
+++ app/api/user/route.js
@@ -0,0 +1,19 @@
+import mongoDBConnection from "@/lib/mongodb/connection";
+import User from "@/models/user";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  console.log("oiasoi")
+
+  const { name, email, provider } = await request.json();
+
+  console.log("hello there")
+
+  await mongoDBConnection();
+
+  console.log("2222")
+
+  await User.create({ name, email, provider });
+
+  return NextResponse.json({ message: "User Registered" }, { status: 201 });
+}
